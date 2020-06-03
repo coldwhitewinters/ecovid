ee7e60d30fae6351d7f9200d2aaf2e3a0f680a94
coldwhitewinters <51274049+coldwhitewinters@users.noreply.github.com>
2020-06-03 01:20:40 -0300

    WIP

--- scripts/SEIR.py
+++ scripts/SEIR.py
@@ -73,12 +73,16 @@ class CovidCalc:
         return result
 
     
-class SIRD:
-    def __init__(self, population=44e6, recovery_rate=0.033, mortality_rate=0.036, beta=0.5):
+class SIR:
+    def __init__(self, population=44e6, transmission_rate=0.1, recovery_rate=0.01, mortality_rate=0.005,
+                 initial_infected=1, initial_recovered=0, initial_dead=0):
         self.population = population
-        self.beta = beta
+        self.transmission_rate = transmission_rate
         self.recovery_rate = recovery_rate
         self.mortality_rate = mortality_rate
+        self.initial_infected = initial_infected
+        self.initial_recovered = initial_recovered
+        self.initial_dead = initial_dead
     
     def _deqn(self, y, t, N, beta, gamma, mu):
         I, R, D = y
@@ -89,10 +93,13 @@ class SIRD:
         dD = mu * I
         return dI, dR, dD
 
-    def simulate(self, y0, until, step=1):
+    def simulate(self, until, step=1):
+        y0 = (self.initial_infected, 
+              self.initial_recovered, 
+              self.initial_dead)
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.beta,
+                      self.transmission_rate,
                       self.recovery_rate,
                       self.mortality_rate,
                      )
@@ -104,14 +111,14 @@ class SIRD:
         for k, param in enumerate(estimate):
             if param in vars(self):
                 vars(self)[param] = theta[k]
-        y0 = obs.head(1).to_numpy().flatten()
-        y = self.simulate(y0=y0, until=len(obs))
+        y = self.simulate(until=len(obs))
         wsqd = weights * (y - obs)**2
         cost = wsqd.sum().sum()
         return cost
     
-    def fit(self, obs, estimate, weights=None, method="nelder-mead"):
+    def fit(self, obs, estimate, weights=None, set_initial=True, method="nelder-mead", options=None):
         obs = obs.reset_index()[["I", "R", "D"]]
+        self.initial_infected, self.initial_recovered, self.initial_dead = obs.head(1).to_numpy().flatten()
         theta_0 = np.zeros(len(estimate))
         for k, param in enumerate(estimate):
             if param not in vars(self):
@@ -120,19 +127,23 @@ class SIRD:
         if weights is None:
             weights = 1/obs.size * np.ones(obs.shape)
         args = (obs, weights, estimate)
-        result = minimize(self._opt_target, theta_0, args=args, method=method)
+        result = minimize(self._opt_target, theta_0, args=args, method=method, options=options)
         return result
 
     
-class SEIRD:
-    def __init__(self, population=44e6, beta=0.05, recovery_rate=0.033, 
-                 incubation_rate=0, mortality_rate=0.036, initial_exposed=0):
+class SEIR:
+    def __init__(self, population=44e6, 
+                 transmission_rate=0.1, progression_rate=0.05, recovery_rate=0.01, mortality_rate=0.005, 
+                 initial_exposed=0, initial_infected=1, initial_recovered=0, initial_dead=0):
         self.population = population
-        self.beta = beta
+        self.transmission_rate = transmission_rate
+        self.progression_rate = progression_rate
         self.recovery_rate = recovery_rate
-        self.incubation_rate = incubation_rate
         self.mortality_rate = mortality_rate
         self.initial_exposed = initial_exposed
+        self.initial_infected = initial_infected
+        self.initial_recovered = initial_recovered
+        self.initial_dead = initial_dead
     
     def _deqn(self, y, t, N, beta, gamma, delta, mu):
         E, I, R, D = y
@@ -144,12 +155,16 @@ class SEIRD:
         dD = mu * I
         return dE, dI, dR, dD
 
-    def simulate(self, y0, until, step=1):
+    def simulate(self, until, step=1):
+        y0 = (self.initial_exposed, 
+              self.initial_infected, 
+              self.initial_recovered, 
+              self.initial_dead)
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.beta,
+                      self.transmission_rate,
                       self.recovery_rate,
-                      self.incubation_rate,
+                      self.progression_rate,
                       self.mortality_rate,
                      )
         result = odeint(self._deqn, y0, t0, args=parameters)
@@ -160,14 +175,16 @@ class SEIRD:
         for k, param in enumerate(estimate):
             if param in vars(self):
                 vars(self)[param] = theta[k]
-        y0 = np.concatenate(([self.initial_exposed], obs.head(1).to_numpy().flatten()))
-        y = self.simulate(y0=y0, until=len(obs))[obs.columns]
+        y = self.simulate(until=len(obs))[obs.columns]
         wsqd = weights * (y - obs)**2
         cost = wsqd.sum().sum()
         return cost
     
-    def fit(self, obs, estimate, weights=None, method="nelder-mead", options=None):
+    def fit(self, obs, estimate, weights=None, initial_exposed=None, method="nelder-mead", options=None):
         obs = obs.reset_index()[["I", "R", "D"]]
+        self.initial_infected, self.initial_recovered, self.initial_dead = obs.head(1).to_numpy().flatten()
+        if initial_exposed is not None:
+            self.initial_exposed = initial_exposed
         theta_0 = np.zeros(len(estimate))
         for k, param in enumerate(estimate):
             if param not in vars(self):
