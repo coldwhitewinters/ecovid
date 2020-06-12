9f52e5c8d6784636b0a0a5798136cb334a715e5b
coldwhitewinters <51274049+coldwhitewinters@users.noreply.github.com>
2020-06-11 21:22:13 -0300

    Cleanup

--- scripts/SEIR.py
+++ scripts/SEIR.py
@@ -6,15 +6,16 @@ from scipy.integrate import odeint
 from scipy.optimize import minimize
     
 class SIR:
-    def __init__(self, population=44e6, transmission_rate=0.1, recovery_rate=0.01, mortality_rate=0.005,
-                 initial_infected=1, initial_recovered=0, initial_dead=0):
+    def __init__(self, population=44e6,
+                 r_transmission=0.1, r_recovery=0.01, r_mortality=0.005,
+                 init_infected=1, init_recovered=0, init_dead=0):
         self.population = population
-        self.transmission_rate = transmission_rate
-        self.recovery_rate = recovery_rate
-        self.mortality_rate = mortality_rate
-        self.initial_infected = initial_infected
-        self.initial_recovered = initial_recovered
-        self.initial_dead = initial_dead
+        self.r_transmission = r_transmission 
+        self.r_recovery = r_recovery
+        self.r_mortality = r_mortality
+        self.init_infected = init_infected
+        self.init_recovered = init_recovered
+        self.init_dead = init_dead
     
     def _deqn(self, y, t, N, beta, gamma, mu):
         I, R, D = y
@@ -26,15 +27,14 @@ class SIR:
         return dI, dR, dD
 
     def simulate(self, until, step=1):
-        y0 = (self.initial_infected, 
-              self.initial_recovered, 
-              self.initial_dead)
+        y0 = (self.init_infected, 
+              self.init_recovered, 
+              self.init_dead)
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.transmission_rate,
-                      self.recovery_rate,
-                      self.mortality_rate,
-                     )
+                      self.r_transmission,
+                      self.r_recovery,
+                      self.r_mortality)
         result = odeint(self._deqn, y0, t0, args=parameters)
         result_df = pd.DataFrame(result, columns=["I", "R", "D"])
         return result_df
@@ -50,7 +50,6 @@ class SIR:
     
     def fit(self, obs, estimate, weights=None, method="nelder-mead", options=None):
         obs = obs.reset_index(drop=True)
-        self.initial_infected, self.initial_recovered, self.initial_dead = obs.head(1).to_numpy().flatten()
         theta_0 = np.zeros(len(estimate))
         for k, param in enumerate(estimate):
             if param not in vars(self):
@@ -64,18 +63,20 @@ class SIR:
 
     
 class SEIR:
-    def __init__(self, population=44e6, 
-                 transmission_rate=0.1, progression_rate=0.05, recovery_rate=0.01, mortality_rate=0.005, 
-                 initial_exposed=0, initial_infected=1, initial_recovered=0, initial_dead=0):
+    def __init__(self, population=44e6,
+                 r_transmission=0.1, r_progression=0.05, 
+                 r_recovery=0.01, r_mortality=0.005, 
+                 init_exposed=0, init_infected=1, 
+                 init_recovered=0, init_dead=0):
         self.population = population
-        self.transmission_rate = transmission_rate
-        self.progression_rate = progression_rate
-        self.recovery_rate = recovery_rate
-        self.mortality_rate = mortality_rate
-        self.initial_exposed = initial_exposed
-        self.initial_infected = initial_infected
-        self.initial_recovered = initial_recovered
-        self.initial_dead = initial_dead
+        self.r_transmission = r_transmission
+        self.r_progression = r_progression
+        self.r_recovery = r_recovery
+        self.r_mortality = r_mortality
+        self.init_exposed = init_exposed
+        self.init_infected = init_infected
+        self.init_recovered = init_recovered
+        self.init_dead = init_dead
     
     def _deqn(self, y, t, N, beta, gamma, delta, mu):
         E, I, R, D = y
@@ -88,16 +89,16 @@ class SEIR:
         return dE, dI, dR, dD
 
     def simulate(self, until, step=1):
-        y0 = (self.initial_exposed, 
-              self.initial_infected, 
-              self.initial_recovered, 
-              self.initial_dead)
+        y0 = (self.init_exposed, 
+              self.init_infected, 
+              self.init_recovered, 
+              self.init_dead)
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.transmission_rate,
-                      self.recovery_rate,
-                      self.progression_rate,
-                      self.mortality_rate)
+                      self.r_transmission,
+                      self.r_recovery,
+                      self.r_progression,
+                      self.r_mortality)
         result = odeint(self._deqn, y0, t0, args=parameters)
         self.result = pd.DataFrame(result, columns=["E", "I", "R", "D"])
         return self.result
@@ -126,21 +127,25 @@ class SEIR:
 
     
 class SEIRH:
-    def __init__(self, population=44e6, hospitalized_rate=0.03, recovery_rate_hosp=0.01,
-                 transmission_rate=0.1, progression_rate=0.05, recovery_rate_mild=0.01, mortality_rate=0.005, 
-                 initial_exposed=0, initial_infected=1, initial_recovered=0, initial_hospitalized=0, initial_dead=0):
+    def __init__(self, population=44e6, 
+                 r_transmission=0.1, r_progression=0.05, 
+                 r_hospitalized=0.05, r_mortality=0.005, 
+                 r_recovery_mild=0.1, r_recovery_hosp=0.01,
+                 init_exposed=0, init_infected=1, 
+                 init_recovered=0, init_hospitalized=0, 
+                 init_dead=0):
         self.population = population
-        self.transmission_rate = transmission_rate
-        self.progression_rate = progression_rate
-        self.recovery_rate_mild = recovery_rate_mild
-        self.hospitalized_rate = hospitalized_rate
-        self.recovery_rate_hosp = recovery_rate_hosp
-        self.mortality_rate = mortality_rate
-        self.initial_exposed = initial_exposed
-        self.initial_infected = initial_infected
-        self.initial_recovered = initial_recovered
-        self.initial_hospitalized = initial_hospitalized
-        self.initial_dead = initial_dead
+        self.r_transmission = r_transmission
+        self.r_progression = r_progression
+        self.r_hospitalized = r_hospitalized
+        self.r_recovery_mild = r_recovery_mild
+        self.r_recovery_hosp = r_recovery_hosp
+        self.r_mortality = r_mortality
+        self.init_exposed = init_exposed
+        self.init_infected = init_infected
+        self.init_recovered = init_recovered
+        self.init_hospitalized = init_hospitalized
+        self.init_dead = init_dead
     
     def _deqn(self, y, t, N, 
               beta, gamma, delta, 
@@ -151,24 +156,24 @@ class SEIRH:
         dE = beta * S * I / N - delta * E
         dI = delta * E - (gamma + eta) * I
         dR = gamma * I + rho * H
-        dH = eta * I - (rho + mu) * H
+        dH = eta * I - (mu + rho) * H
         dD = mu * H
         return dE, dI, dR, dH, dD
 
     def simulate(self, until, step=1):
-        y0 = (self.initial_exposed, 
-              self.initial_infected, 
-              self.initial_recovered,
-              self.initial_hospitalized,
-              self.initial_dead) 
+        y0 = (self.init_exposed, 
+              self.init_infected, 
+              self.init_recovered,
+              self.init_hospitalized,
+              self.init_dead) 
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.transmission_rate, 
-                      self.recovery_rate_mild,
-                      self.progression_rate,
-                      self.hospitalized_rate,
-                      self.recovery_rate_hosp,
-                      self.mortality_rate)
+                      self.r_transmission, 
+                      self.r_recovery_mild,
+                      self.r_progression,
+                      self.r_hospitalized,
+                      self.r_recovery_hosp,
+                      self.r_mortality)
         result = odeint(self._deqn, y0, t0, args=parameters)
         
         columns_names = ["E", "I", "R", "H", "D"]
@@ -198,26 +203,29 @@ class SEIRH:
         return result
     
 class SEIARH:
-    def __init__(self, population=44e6, hospitalized_rate=0.03, recovery_rate_hosp=0.01,
-                 transmission_rate_s=0.1, transmission_rate_a=0.1, progression_rate=0.05, 
-                 recovery_rate_mild=0.01, mortality_rate=0.005, asymptomatic_rate=0.5,
-                 initial_exposed=0, initial_infected=1, initial_recovered=0, 
-                 initial_hospitalized=0, initial_dead=0, initial_asymptomatic=0):
+    def __init__(self, population=44e6,  
+                 r_transmission_s=0.1, r_transmission_a=0.1, 
+                 r_progression=0.05, r_asymptomatic=0.5,
+                 r_hospitalized=0.05, r_mortality=0.005, 
+                 r_recovery_mild=0.01, r_recovery_hosp=0.01,  
+                 init_exposed=0, init_infected=1, 
+                 init_recovered=0, init_hospitalized=0, 
+                 init_dead=0, init_asymptomatic=0):
         self.population = population
-        self.asymptomatic_rate = asymptomatic_rate
-        self.transmission_rate_s = transmission_rate_s
-        self.transmission_rate_a = transmission_rate_a
-        self.progression_rate = progression_rate
-        self.recovery_rate_mild = recovery_rate_mild
-        self.hospitalized_rate = hospitalized_rate
-        self.recovery_rate_hosp = recovery_rate_hosp
-        self.mortality_rate = mortality_rate
-        self.initial_exposed = initial_exposed
-        self.initial_infected = initial_infected
-        self.initial_asymptomatic = initial_asymptomatic
-        self.initial_recovered = initial_recovered
-        self.initial_hospitalized = initial_hospitalized
-        self.initial_dead = initial_dead
+        self.r_asymptomatic = r_asymptomatic
+        self.r_transmission_s = r_transmission_s
+        self.r_transmission_a = r_transmission_a
+        self.r_progression = r_progression
+        self.r_recovery_mild = r_recovery_mild
+        self.r_hospitalized = r_hospitalized
+        self.r_recovery_hosp = r_recovery_hosp
+        self.r_mortality = r_mortality
+        self.init_exposed = init_exposed
+        self.init_infected = init_infected
+        self.init_asymptomatic = init_asymptomatic
+        self.init_recovered = init_recovered
+        self.init_hospitalized = init_hospitalized
+        self.init_dead = init_dead
     
     def _deqn(self, y, t, N, 
               alpha, beta_s, beta_a, gamma, delta, 
@@ -233,22 +241,22 @@ class SEIARH:
         return dE, dI, dA, dR, dH, dD
 
     def simulate(self, until, step=1):
-        y0 = (self.initial_exposed, 
-              self.initial_infected,
-              self.initial_asymptomatic,
-              self.initial_recovered,
-              self.initial_hospitalized,
-              self.initial_dead) 
+        y0 = (self.init_exposed, 
+              self.init_infected,
+              self.init_asymptomatic,
+              self.init_recovered,
+              self.init_hospitalized,
+              self.init_dead) 
         t0 = np.arange(0, until, step)
         parameters = (self.population,
-                      self.asymptomatic_rate,
-                      self.transmission_rate_s,
-                      self.transmission_rate_a,
-                      self.recovery_rate_mild,
-                      self.progression_rate,
-                      self.hospitalized_rate,
-                      self.recovery_rate_hosp,
-                      self.mortality_rate)
+                      self.r_asymptomatic,
+                      self.r_transmission_s,
+                      self.r_transmission_a,
+                      self.r_recovery_mild,
+                      self.r_progression,
+                      self.r_hospitalized,
+                      self.r_recovery_hosp,
+                      self.r_mortality)
         result = odeint(self._deqn, y0, t0, args=parameters)
         
         columns_names = ["E", "I", "A", "R", "H", "D"]
